eb0661211e2849ef57b9bd0de4e3b8514ea0351a
solen <solen0918@gmail.com>
2025-02-28 19:28:26 +0700

    practice_react

---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+export default function CardComponent({ Books }) {
+  const [bookList, setBookList] = useState([]);
+
+  const mergeStudents = () => {
+    setBookList([...Books]);
+  };
+
+  useEffect(() => {
+    mergeStudents();
+  }, [Books]);
+  return (
+    <div className="grid grid-cols-3 gap-6">
+      {bookList?.map((book, index) => (
+        <div key={index} className=" bg-gray-200 rounded-lg shadow-lg">
+          <p className="float-right bg-blue-500 p-2 rounded-l-lg text-white">
+            {book?.publishedDate}
+          </p>
+          <div className="py-4 px-5 space-y-2 mt-6">
+            {" "}
+            <h1 className="text-4xl font-medium">{book?.bookTitle}</h1>
+            <p className="text-lg"> Written by :{book?.authorName}</p>
+            <p className="text-lg">Editon : {book?.edition}</p>
+            <p className="text-lg">Quantity : {book?.quantity}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/InputComponent.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import CardComponent from "./CardComponent";
+
+export default function InputComponent() {
+  const [book, setBook] = useState({});
+  const [Books, setBooks] = useState([]);
+
+  // handle input change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    // console.log("helo" + e.target.name);
+
+    setBook((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  useEffect(() => {
+    console.log(book);
+    console.log(Books);
+  });
+
+  //   handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setBooks((prev) => [...prev, { ...book }]);
+  };
+  return (
+    <div className="container mx-auto my-6 space-y-6">
+      {/* header */}
+      <h1 className="text-3xl font-semibold text-center">React Project Demo</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="grid gap-6 mb-6 md:grid-row-2">
+          <div className="grid grid-cols-2 gap-6">
+            <div>
+              <label
+                htmlFor="bookTitle"
+                className="block mb-2 text-lg font-medium text-gray-900 ">
+                Book Title
+              </label>
+              <input
+                type="text"
+                name="bookTitle"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="John"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="authorName"
+                className="block mb-2 text-lg font-medium text-gray-900 ">
+                Author
+              </label>
+              <input
+                type="type"
+                name="authorName"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Jonh smith"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-6">
+            {" "}
+            {/* Date */}
+            <div>
+              <label
+                htmlFor="publishedDate"
+                className="block mb-2 text-lg font-medium text-gray-900 ">
+                Published Date
+              </label>
+              <input
+                type="date"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Select date"
+                name="publishedDate"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="-col-span-2">
+              <label
+                htmlFor="edition"
+                className="block mb-2 text-lg font-medium text-gray-900 ">
+                Edition
+              </label>
+              <input
+                type="number"
+                name="edition"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="1"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="quantity"
+                className="block mb-2 text-lg font-medium text-gray-900 ">
+                Quantity
+              </label>
+              <input
+                type="number"
+                name="quantity"
+                className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="1"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Submit
+        </button>
+      </form>
+      <CardComponent Books={Books} />
+    </div>
+  );
+}
