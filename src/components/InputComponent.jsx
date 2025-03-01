3922395b2bc909ed24dd90f6284209d92811a53a
solen <solen0918@gmail.com>
2025-03-01 22:07:49 +0700

    update1

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -79,7 +79,7 @@ export default function InputComponent() {
                 className="block mb-2 text-lg font-medium text-gray-900 ">
                 Published Date
               </label>
-              <div class="relative max-w-sm">
+              <div class="relative">
                 <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                   <svg
                     class="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -93,7 +93,7 @@ export default function InputComponent() {
                 <input
                   datepicker
                   id="default-datepicker"
-                  type="text"
+                  type="date"
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Select date"
                 />
